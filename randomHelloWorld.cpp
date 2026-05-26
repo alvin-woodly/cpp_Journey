f1fd1ed22d5ba6a1ad4bf5c753a6d67df99d0690
alvin-woodly <alvinwoodly@gmail.com>
2026-05-25 22:27:45 -0300

    randomHelloWorld updated with some input verification

--- randomHelloWorld.cpp
+++ randomHelloWorld.cpp
@@ -10,20 +10,40 @@
 
 #include <iostream>
 #include <string_view>
+#include <string>
 #include <random>
 #include <limits>
+#include <cctype>
 
 namespace{
 
     //constexpr because the value is known at compile time
     constexpr std::string_view choices [2] {"Hello","World"};
+
+    bool isNumber(std::string_view userInput);
+    
 }
 
 int main()
 {
-    std::cout <<"How many times would you like to roll?" << '\n';
-    int rolls{};
-    std::cin >> rolls;
+    
+    int int_rolls{};
+    std::string rolls{};
+    
+    //if input fails reset the operation mode to normal
+    if(!std::cin)
+    {
+        std::cin.clear();
+    }
+    
+    do{
+        std::cout <<"How many times would you like to roll?" << '\n';
+        std::cout <<"please enter numbers only " << '\n';
+        std::cin >> rolls;
+    }
+    while(!isNumber(rolls));
+
+    int_rolls = std::stoi(rolls);
 
     //clear input buffer
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
@@ -41,9 +61,26 @@ int main()
     std::uniform_int_distribution dist{0,1};
 
     //print either "Hello" or "World" an X amount of times
-    for(int i=0;i < rolls; i++)
+    for(int i=0;i < int_rolls; i++)
     {
         std::cout << choices[dist(mt)] << '\n';
     }
     return 0;
+}
+
+
+namespace{
+
+    bool isNumber(std::string_view userInput)
+    {
+        for(char c : userInput)
+        {
+            if(!std::isdigit(c))
+            {
+                return false;
+            }
+        }
+    
+        return true;
+    }
 }
